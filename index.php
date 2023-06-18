3179cf4354d843fa9594616b17d30fff0890175e
Arjun Khatri <105587143+4kxcode@users.noreply.github.com>
2023-06-18 15:07:41 +0545

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,216 @@
 
+
+<!Doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
+
+    <title>Arjun Khatri</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+    <link rel="shortcut icon" href="/media/favicon/favicon.ico" type="image/x-icon">
+    <script type="text/javascript" src="https://me.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=k8_ptPNSRrSoIirXYtJfDjqrOOSfTyUnGv_V_sIZbbhuQWglCE3OKosIVe6pUB0Z" charset="UTF-8"></script><link rel="stylesheet" crossorigin="anonymous" href="https://me.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cHM6Ly9hcmp1bmtoYXRyaS5jb20ubnAv"/><script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "h7xh5oo77t");
+    </script>
+    <style>
+        body{
+
+        }
+        /*     General overwrite     */
+        a{
+            color: #2C93FF;
+        }
+        a:hover, a:focus {
+            color: #1084FF;
+        }
+        a:focus, a:active,
+        button::-moz-focus-inner,
+        input[type="reset"]::-moz-focus-inner,
+        input[type="button"]::-moz-focus-inner,
+        input[type="submit"]::-moz-focus-inner,
+        select::-moz-focus-inner,
+        input[type="file"] > input[type="button"]::-moz-focus-inner {
+            outline : 0;
+        }
+
+        /*           Typography          */
+
+        p{
+            font-size: 16px;
+            line-height: 1.6180em;
+        }
+
+        .main{
+            background-image: url('/media/bg/bg.jpg');
+            background-position: center center;
+            background-size: cover;
+            height: auto;
+            left: 0;
+            min-height: 100%;
+            min-width: 100%;
+            position: absolute;
+            top: 0;
+            width: auto;
+        }
+        .cover{
+            position: fixed;
+            opacity: 1;
+            background-color: rgba(0,0,0,.6);
+            left: 0px;
+            top: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: 1;
+        }
+        .cover.black{
+            background-color: rgba(0,0,0,.6);
+        }
+
+        .logo-container .logo{
+            overflow: hidden;
+            border-radius: 50%;
+            border: 1px solid #333333;
+            width: 60px;
+            float: left;
+        }
+
+        .main .logo{
+            color: #FFFFFF;
+            font-size: 56px;
+            font-weight: 300;
+            position: relative;
+            text-align: center;
+            text-shadow: 0 0 10px rgba(0, 0, 0, 0.33);
+            margin-top: 150px;
+            z-index: 3;
+        }
+        .main .logo.cursive{
+            font-family: 'Grand Hotel',cursive;
+            font-size: 82px;
+
+        }
+        .main .content{
+            position: relative;
+            z-index: 4;
+        }
+        .main .motto{
+            min-height: 140px;
+        }
+        .main .motto, .main .subscribe .info-text{
+            font-size: 28px;
+            color: #FFFFFF;
+            text-shadow: 0 1px 4px rgba(0, 0, 0, 0.33);
+            text-align: center;
+            margin-top: 50px;
+
+        }
+        .main .subscribe .info-text{
+            font-size: 18px;
+            margin-bottom: 30px;
+        }
+
+        .footer{
+            position: relative;
+            bottom: 20px;
+            right: 0;
+            width: 100%;
+            color: #AAAAAA;
+            z-index: 4;
+            text-align: right;
+            margin-top: 50px;
+        }
+        .footer a{
+            color: #FFFFFF;
+        }
+
+        @media (min-width:991px){
+            .footer{
+                position: fixed;
+                bottom: 20px;
+            }
+        }
+
+    </style>
+
+    <!--     Fonts     -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+<div class="main">
+    <div class="cover black" data-color="black"></div>
+    <div class="container">
+        <h1 class="logo cursive">
+            Everthing About Arjun Khatri
+        </h1>
+
+        <div class="content">
+            <h4 class="motto">Coming Soon...</h4>
+            <div class="subscribe">
+                <h5 class="info-text">
+                    Click <a href="https://arjunkhatri.com.np/about/a1/u1/" target="_blank">About</a> to know Arjun Khatri or 
+                    click <a href="/subscribe" target="_blank">Subscribe</a> or <a href="https://daecf6ec.sibforms.com/serve/MUIEAEZ-U7Jz3NizkyOmSfw05UuqnN40vi_uhDRBrSz87rqy360tAjRGAK9QSmyqByPWwZBLkV72cNNzIXtNPpuveTFM6OSWl7-BQ9XkxRckMUjQzX4XExTzC5BeKFOwJa1kwQ6XHqKKgUilKSmRZdDPisrKY1hV9RjnTlhwBXqJWGNOTIgZL9hXJb1uCtjcwoOi4AvXUH6FXfdF" target="_blank">Unsubscribe</a> to the Newsletters from Arjun Khatri.
+                </h5>
+            </div>
+
+
+
+
+ <script src="https://www.google.com/recaptcha/api.js"></script>
+ <script>
+   function onSubmit(token) {
+     document.getElementById("demo-form").submit();
+   }
+ </script>
+<!-- <button class="g-recaptcha" 
+        data-sitekey="reCAPTCHA_site_key" 
+        data-callback='onSubmit' 
+        data-action='submit'>Submit</button> -->
+        </div>
+    </div>
+    <div class="footer">
+        <div class="container">
+            &copy;<label id="current_year"></label> &reg;ArjunKhatri&trade;<br>
+            <a href="https://arjunkhatri.com.np/"><img src="/media/bg/bglogo.jpg" alt="Arjun Khatri" height="30px"></a>
+        </div>
+
+<!-- Sendinblue Conversations {literal} -->
+<script>
+    (function(d, w, c) {
+        w.SibConversationsID = '63d2556d96fc5747c14ba684';
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        var s = d.createElement('script');
+        s.async = true;
+        s.src = 'https://conversations-widget.sendinblue.com/sib-conversations.js';
+        if (d.head) d.head.appendChild(s);
+    })(document, window, 'SibConversations');
+</script>
+<!-- /Sendinblue Conversations {/literal} -->
+    </div>
+</div>
+
+
+
+
+<script>
+    date = new Date();
+    year = date.getFullYear();
+    document.getElementById("current_year").innerHTML = +year;
+</script>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" type="text/javascript"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
+
+</body>
+</html>
